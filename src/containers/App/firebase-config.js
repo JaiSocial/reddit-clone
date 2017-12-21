1033f7dd50bd1a7fd3892ecbfb931925cb29a7fa
Jay Sundaram <jsocial2020@gmail.com>
2017-12-21 18:46:50 -0500

    initial check-in

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+	apiKey: "AIzaSyDlrpoRJQoMT7KbRl438m6KCFEz56cbX_U",
+	authDomain: "reddit-clone-e1469.firebaseapp.com",
+	databaseURL: "https://reddit-clone-e1469.firebaseio.com",
+	projectId: "reddit-clone-e1469",
+	storageBucket: "reddit-clone-e1469.appspot.com",
+	messagingSenderId: "998196385156"
+};
